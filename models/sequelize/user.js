a03cfb1e7edaad0598e7771c898195bcea79276c
Sunil More <sunilmore690@gmail.com>
2018-07-11 22:20:02 +0530

    add sequelize user

--- models/sequelize/user.js
+++ models/sequelize/user.js
@@ -20,30 +20,3 @@ module.exports = {
       type: Sequelize.STRING
     }
   }
-const User = sequelizer.define("USER", {
-  login: {
-    type: Sequelize.STRING
-  },
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  },
-  company_id: {
-    type: Sequelize.INTEGER
-  },
-  userId: {
-    type: Sequelize.INTEGER,
-    primaryKey: true
-  },
-  sig_mobile: {
-    type: Sequelize.STRING
-  }
-});
-
-// User.hasOne(Company, {
-//     foreignKey: 'company_id'
-// })
-
-module.exports = User;
